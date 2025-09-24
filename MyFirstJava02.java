e758b0cd6729d86dca7a841ced0f2a738c1ee4e2
lanabuwaik <lanabuwaik@gmail.com>
2025-09-24 13:48:54 +0700

    commit

---
+++ MyFirstJava02.java
@@ -0,0 +1,27 @@
+public class MyFirstJava02 {
+    public static void main (String[] args) {
+    System.out.println("254107060054\tAji\tSIB");
+    System.out.println("254107060126\tDaffa\tSIB");
+    System.out.println("254107060095\tAlief\tSIB");
+    System.out.println("254107060135\tGalih\tSIB");
+    System.out.println("254107060092\tFirdauzy\tSIB");
+    System.out.println("254107060161\tRobby\tSIB");
+    System.out.println("254107060074\tAdzriel\tSIB");
+    System.out.println("254107060060\tAliyah\tSIB");
+    System.out.println("254107060143\tAlifia\tSIB");
+    System.out.println("254107060053\tRevalinda\tSIB");
+    System.out.println("254107060155\tSaum\tSIB");
+    System.out.println("254107060129\tAlya\tSIB");
+    System.out.println("254107060109\tLovy\tSIB");
+    System.out.println("254107060115\tSilmi\tSIB");
+    System.out.println("254107060166\tLana\tSIB");
+    System.out.println("254107060083\tRasya\tSIB");
+    System.out.println("244107060163\tRangga\tSIB");
+    System.out.println("254107060054\tAlberto\tSIB");
+    System.out.println("254107060094\tNabila\tSIB");
+    System.out.println("254107060154\tDeizan\tSIB");
+    System.out.println("254107060149\tRada\tSIB");
+    System.out.println("254107060151\tHilma\tSIB");
+
+    }
+}
